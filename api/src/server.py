dee1eaf92007583abb55ed6bdc4c112c1587451d
Lbrzr <akakpoleandre0@gmail.com>
2023-10-11 13:01:43 +0200

    cors issue managed

--- api/src/server.py
+++ api/src/server.py
@@ -8,7 +8,7 @@ from aiohttp import web
 import socketio
 
 # create a Socket.IO server
-sio = socketio.AsyncServer()
+sio = socketio.AsyncServer(cors_allowed_origins='*')
 
 # wrap with ASGI application
 # app = socketio.ASGIApp(sio)
